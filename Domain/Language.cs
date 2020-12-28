0c870a41dca0ee19683c969f8d8db889a4558ef3
santinodenbrave <santinodenbrave@protonmail.com>
2020-12-28 15:32:52 +0100

    added domain layer

---
+++ Domain/Language.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lingowords.Domain
+{
+    public class Word
+    {
+        public string word { get;}
+        public int length { get; }
+        public string lang { get; }
+
+        public Word(string _word)
+        {
+            word = _word;
+        }
+
+        public Word(string _word, int _length, string _lang)
+        {
+            word = _word;
+            length = _length;
+            lang = _lang;
+        }
+    }
+}
